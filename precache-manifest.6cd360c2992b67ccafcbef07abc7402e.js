35fd8b61d7af84c95e54d4a71731cdd37a473390
alexfigliolia <alexfigliolia@gmail.com>
2019-05-20 11:13:36 -0700

    Upload build

---
+++ precache-manifest.6cd360c2992b67ccafcbef07abc7402e.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "98da70e595bc329a0bfbc35ef080e355",
+    "url": "/portfolio-vue/img/nightsky.98da70e5.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio-vue/robots.txt"
+  },
+  {
+    "revision": "0e44be243e57c8d329909ad9caedd360",
+    "url": "/portfolio-vue/img/sv-small.0e44be24.jpg"
+  },
+  {
+    "revision": "32043c9e894528c6f60c",
+    "url": "/portfolio-vue/css/chunk-21ad4668.d4636020.css"
+  },
+  {
+    "revision": "93e5ffe7466237a1aa68",
+    "url": "/portfolio-vue/css/chunk-9d060a2e.8f954554.css"
+  },
+  {
+    "revision": "93e5ffe7466237a1aa68",
+    "url": "/portfolio-vue/js/chunk-9d060a2e.c4abe481.js"
+  },
+  {
+    "revision": "0ebf2c9f5726cb17cc5e",
+    "url": "/portfolio-vue/js/chunk-vendors.d6226985.js"
+  },
+  {
+    "revision": "e3583ba4ecedb87b971ce24979722331",
+    "url": "/portfolio-vue/img/sv-large.e3583ba4.jpg"
+  },
+  {
+    "revision": "bbf6c7c6297a97dd86f9533c01757510",
+    "url": "/portfolio-vue/img/republic-small.bbf6c7c6.jpg"
+  },
+  {
+    "revision": "ebb0edcb063f27c8e384a4d20ea14b0c",
+    "url": "/portfolio-vue/img/cityweb.ebb0edcb.jpg"
+  },
+  {
+    "revision": "e5c0925158bb45f7a43dd17d8f0c27d6",
+    "url": "/portfolio-vue/img/nightsky-small.e5c09251.jpg"
+  },
+  {
+    "revision": "19cf8ff218cdd8f51459bdf9e9ddd89e",
+    "url": "/portfolio-vue/img/dog.19cf8ff2.jpg"
+  },
+  {
+    "revision": "1cf802527aea906b0e50f45dc5b12775",
+    "url": "/portfolio-vue/img/react-small.1cf80252.jpg"
+  },
+  {
+    "revision": "076e386f209bc23501d96c04c731bccd",
+    "url": "/portfolio-vue/img/gnocchi.076e386f.jpg"
+  },
+  {
+    "revision": "57dc6a2b6d813b7aa54d1f9190548b71",
+    "url": "/portfolio-vue/img/winter1.57dc6a2b.jpg"
+  },
+  {
+    "revision": "32463de5a46bef76762a262b51af83bb",
+    "url": "/portfolio-vue/img/gnocchi-small.32463de5.jpg"
+  },
+  {
+    "revision": "f0afd889c6fac60edac122a7f70f2f2e",
+    "url": "/portfolio-vue/img/wordClouds.f0afd889.png"
+  },
+  {
+    "revision": "85bbbb3f51c600a6fff033404e7bb5bc",
+    "url": "/portfolio-vue/img/tree-small.85bbbb3f.jpg"
+  },
+  {
+    "revision": "d16d2c4022be4c5f842b",
+    "url": "/portfolio-vue/js/app.060a3597.js"
+  },
+  {
+    "revision": "32043c9e894528c6f60c",
+    "url": "/portfolio-vue/js/chunk-21ad4668.2aa4e942.js"
+  },
+  {
+    "revision": "b5fe43c6fd3a0bdcb1be097ce3ad7c48",
+    "url": "/portfolio-vue/img/winter3.b5fe43c6.jpg"
+  },
+  {
+    "revision": "fc6246858fc67310e0f9fc4b80d2ae55",
+    "url": "/portfolio-vue/img/cityweb-small.fc624685.jpg"
+  },
+  {
+    "revision": "bb8d483fb06e8b82177e53af2129a411",
+    "url": "/portfolio-vue/img/skedge-desktop2.bb8d483f.jpg"
+  },
+  {
+    "revision": "bfdc87f37e3e74c617440ba527f497cd",
+    "url": "/portfolio-vue/img/wordClouds-small.bfdc87f3.png"
+  },
+  {
+    "revision": "f06e307b29a675d7968fc14a69763b21",
+    "url": "/portfolio-vue/img/tree.f06e307b.jpg"
+  },
+  {
+    "revision": "3214b642ff78d264e13edd0872840e69",
+    "url": "/portfolio-vue/img/COMPR-sm.3214b642.jpg"
+  },
+  {
+    "revision": "75c30d9398bb2025b2b6ebb07303b750",
+    "url": "/portfolio-vue/img/COMPR.75c30d93.jpg"
+  },
+  {
+    "revision": "e45e76ce18b415a483a2caf3a277b706",
+    "url": "/portfolio-vue/img/down.e45e76ce.svg"
+  },
+  {
+    "revision": "e461c001ca7480a9d5fe7073cc050383",
+    "url": "/portfolio-vue/img/left.e461c001.svg"
+  },
+  {
+    "revision": "79d2b7b362c5a64716ac8ac0cb9063c3",
+    "url": "/portfolio-vue/img/right.79d2b7b3.svg"
+  },
+  {
+    "revision": "de89b74962e758cea409d1fa6379fc44",
+    "url": "/portfolio-vue/img/up.de89b749.svg"
+  },
+  {
+    "revision": "d65ffcf0b35cfb3a8c4ffdcd9faa7a01",
+    "url": "/portfolio-vue/img/republic-large.d65ffcf0.jpg"
+  },
+  {
+    "revision": "517efdab99ceb6802093340cf408b045",
+    "url": "/portfolio-vue/img/react-desktop2.517efdab.jpg"
+  },
+  {
+    "revision": "b4692dee2ee1a651775c2165a0188c0f",
+    "url": "/portfolio-vue/img/dog-small.b4692dee.jpg"
+  },
+  {
+    "revision": "fc7330064457091f65e77ff41d38eb38",
+    "url": "/portfolio-vue/img/skedge-small.fc733006.jpg"
+  },
+  {
+    "revision": "720c8cb9e8a490b012bb665c196b0680",
+    "url": "/portfolio-vue/index.html"
+  },
+  {
+    "revision": "d16d2c4022be4c5f842b",
+    "url": "/portfolio-vue/css/app.a226d959.css"
+  }
+];
